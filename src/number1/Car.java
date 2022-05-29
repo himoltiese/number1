e314ef5542ce3559adf8fbc0dacf5faa9dca00c1
molcat <qqq3964@naver.com>
2022-05-29 23:32:21 +0900

    Initial commit

---
+++ src/number1/Car.java
@@ -0,0 +1,10 @@
+package number1;
+
+public class Car {
+    public static void main(String[] args) {
+        Hankook hk = new Hankook();
+        hk.print();
+        hk.speed=1;
+        System.out.println(hk.speed);
+    }
+}
---
+++ src/number1/Hankook.java
@@ -0,0 +1,8 @@
+package number1;
+
+public class Hankook {
+    public int speed;
+    public void print(){
+        System.out.println("hello");
+    }
+}
